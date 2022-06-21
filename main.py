5eff6482b8e22306a7c3ed8cb9facf5fe0ad61a1
Li Yang <alphonsusho10s@gmail.com>
2022-06-21 22:41:40 +0800

    running the modules here

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import cv2
-import numpy as np
+from concurrent.futures import process
+from common import config
+from src.runner import scanRunner as scanRunner
 
-from src.scanPlanner.imageProcessor import imageProcessor
-from src.scanPlanner.extractCoord import DataCleaner, Clustering
+### Loading up the configs
+def imageProcessor_loader():
+    return config.imageProcessor
 
+def extractCoord_loader():
+    return config.extractCoord
 
+def main():
+    extractedCoordinates = scanRunner(
+        processor=imageProcessor_loader(),
+        cleaner=extractCoord_loader())
 
+    # TODO run scanPlanner
+    # TODO run pathPlanner
 
-        
-
-
-
-
+if __name__ == "__main__":
+    main()
