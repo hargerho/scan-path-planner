97879f620391e039130c270c0779d9eeb0ce3e2c
Li Yang <alphonsusho10s@gmail.com>
2022-06-21 22:42:55 +0800

    rearranging files

---
+++ common/config.py
@@ -0,0 +1,18 @@
+imageProcessor = {
+    "image": 'maps/jpegfloorplan.jpg',
+    "min_pixel_size": 0, # minimum size of particles we want to keep
+    "noise_removal_threshold": 25, # Minimal area of blobs to be kept
+    "corners_threshold": 0.1, # Threshold to allow corners. Higher removes more of the house
+    "room_closing_max_length": 100, # Maximum line length to add to close off open doors
+    "gap_in_wall_threshold": 500, # Minimum number of pixels to identify component as room instead of hole in the wall
+    "binary_threshold": 128, # If pixel falls above/below threshold, make it white/black
+    "x_threshold": 42, # number of x-axis pixels to close up the gap between 2 points on x-axis
+    "y_threshold": 55, # number of y-axis pixels to close up the gap between 2 points on y-axis
+}
+
+scanPlanner = {
+    "DBSCAN": True, # Activate DBSCAN clustering module
+    "KMeans": False, # Activate KMeans clustering module
+    "maxRange": 2000, # Max laser range
+    "minRange": 24, # Min laser range
+}
---
+++ src/runner/scanRunner.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+from src.scanPlanner.imageProcessor import imageProcessor
+from src.scanPlanner.extractCoord import DataCleaner, Clustering
+
+
+class ScanPlan:
+    def __init__(self, processor, cleaner):
+        self.imageProcessor = imageProcessor(processor=processor)
+        self.DataCleaner = DataCleaner(cleaner=cleaner)
