ac9f3730799d632d7d0e4b892e5dae641fbc1168
Li Yang <alphonsusho10s@gmail.com>
2022-06-22 23:02:03 +0800

    cleaning up

--- src/runner/scanRunner.py
+++ src/runner/scanRunner.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 
+# Importing the modules
 from src.scanPlanner.imageProcessor import imageProcessor
 from src.scanPlanner.extractCoord import DataCleaner, Clustering
 
 
 class ScanRunner:
     def __init__(self, processor, cleaner):
+
+        
+
+        # Initalise the modules
         self.imageProcessor = imageProcessor(processor=processor)
         self.DataCleaner = DataCleaner(cleaner=cleaner)
