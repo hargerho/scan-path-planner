fb4ad78163a15277cd2af8e4b358ba6ddca8dcf0
Li Yang <alphonsusho10s@gmail.com>
2022-07-12 22:37:09 +0800

    adding modules to the runner class

--- src/runner/scanRunner.py
+++ src/runner/scanRunner.py
@@ -9,13 +9,23 @@ from src.scanPlanner.extractCoord import DataCleaner, Clustering
 class ScanRunner:
     def __init__(self, processor, cleaner):
 
-        
-
         # Initalise the modules
         self.imageProcessor = imageProcessor(processor=processor)
         self.DataCleaner = DataCleaner(cleaner=cleaner)
-
-    def extracCoord(self):
-        self.DataCleaner.distanceCalculator
-
     
+    def imageProcesing(self):
+        self.cleanedImg = self.imageProcessor.remove_annotation()
+        self.coloredImg, self.rooms = self.imageProcessor.colour_rooms(self.cleanedImg)
+        return self.imageProcessor.roomCoordinates(self.coloredImg, self.rooms)
+
+    def extracCoord(self, scanCandidates, wall):
+        self.eligibleCandidates = self.DataCleaner.distanceCalculator(scan_candidates=scanCandidates, wall=wall)
+        return self.DataCleaner.dataCleaner(eligible_candidates=self.eligibleCandidates)
+
+    def ScanRun(self):
+
+        self.rooms_coord, self.wall_dict = self.imageProcesing()
+
+        self.cleanDict = self.extracCoord(scanCandidates=self.rooms_coord, wall=self.wall_dict)
+
+        return self.cleanDict
