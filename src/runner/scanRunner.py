744ff02f7fe2daed555e9d23f9401e098d8a8f38
Li Yang <alphonsusho10s@gmail.com>
2022-07-16 00:16:40 +0800

    pushing

--- src/runner/scanRunner.py
+++ src/runner/scanRunner.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Importing the modules
-from src.scanPlanner.imageProcessor import imageProcessor
+from scanPlanner.ImageProcessor import ImageProcessor
 from src.scanPlanner.extractCoord import DataCleaner, Clustering
 
 
@@ -10,22 +10,22 @@ class ScanRunner:
     def __init__(self, processor, cleaner):
 
         # Initalise the modules
-        self.imageProcessor = imageProcessor(processor=processor)
+        self.ImageProcessor = ImageProcessor(processor=processor)
         self.DataCleaner = DataCleaner(cleaner=cleaner)
     
     def imageProcesing(self):
-        self.cleanedImg = self.imageProcessor.remove_annotation()
-        self.coloredImg, self.rooms = self.imageProcessor.colour_rooms(self.cleanedImg)
-        return self.imageProcessor.roomCoordinates(self.coloredImg, self.rooms)
+        self.cleanedImg = self.ImageProcessor.remove_annotation()
+        self.coloredImg, self.rooms = self.ImageProcessor.colour_rooms(self.cleanedImg)
+        return self.ImageProcessor.roomCoordinates(self.coloredImg, self.rooms)
 
     def extracCoord(self, scanCandidates, wall):
         self.eligibleCandidates = self.DataCleaner.distanceCalculator(scan_candidates=scanCandidates, wall=wall)
         return self.DataCleaner.dataCleaner(eligible_candidates=self.eligibleCandidates)
 
-    def ScanRun(self):
+    def scanRun(self):
 
         self.rooms_coord, self.wall_dict = self.imageProcesing()
 
         self.cleanDict = self.extracCoord(scanCandidates=self.rooms_coord, wall=self.wall_dict)
 
-        return self.cleanDict
+        return list(self.cleanDict)
